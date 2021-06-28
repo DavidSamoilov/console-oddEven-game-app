617582405aa80d27771433d085118fc829c30025
David <dada7117@gmail.com>
2021-06-28 14:12:08 +0300

    phase 1 working beeds a lot of cleaning

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import randomNumber from "./JavaScript/createRandomNumber.js";
 
 const playerNamesArr = [];
 
+// Tracking Points
+const playersPoints = {};
+
 const setPlayerNames = (playerAmount) => {
   for (let index = 1; index <= playerAmount; index++) {
     let curName = prompt(`player ${index} name`);
@@ -20,9 +23,7 @@ let currentPlayersPlaying = playerNamesArr;
 
 console.log(
   `Game starting , ${playerNamesArr[0]}(even) against ${playerNamesArr[1]}(odd) `
-);
-
-const playersPoints = {};
+  );
 
 const addPoint = (playerName) => {
     if(playersPoints[playerName]){
@@ -37,26 +38,37 @@ const addPoint = (playerName) => {
 const checkIfPlayerWon = (playerName,pointsNeededToWin,playersPointsObj) => {
     if(playersPointsObj[playerName] >= pointsNeededToWin) {
         console.log(`${playerName} is the winner`);
-        return
+        return true
     }
 }
 
-const Round = (currentPlayersPlaying,NumberOfRoundsToWin) => {
-  const curRandomNum = randomNumber(-5, 13);
-  curRandomNum % 2 === 0
-    ? (addPoint(currentPlayersPlaying[0]),
-    checkIfPlayerWon(currentPlayersPlaying[0],2))
-    : (addPoint(currentPlayersPlaying[1]),
-    checkIfPlayerWon(currentPlayersPlaying[1],2))
+const getCurrentRoundWinner = (player1,player2,curRandomNum) => {
+    if(curRandomNum % 2 === 0){
+    addPoint(player1);
+    return player1;
+    }else{
+        addPoint(player2);
+        return player2;
+    }
 
-};
+}
 
-const FullMatch = (NumberOfRoundsToWin,playerNamesArr) => {
-    while(true){
-        Round(playerNamesArr,NumberOfRoundsToWin){
 
+const FullMatch = (NumberOfRoundsToWin,playerNamesArr,playersPoints,minRandom,maxRandom) => {
+    let [player1,player2] = playerNamesArr
+    let winnerWasFound = false
+    let RoundNumber = 1
+    while(winnerWasFound === false){
+        const curRandomNum = randomNumber(minRandom, maxRandom);
+        let roundWinner = getCurrentRoundWinner(player1,player2,curRandomNum)
+        console.log(`#${RoundNumber}ROUND, random number is ${curRandomNum}`);
+        if(checkIfPlayerWon(roundWinner,NumberOfRoundsToWin,playersPoints)) {
+            winnerWasFound = true
         }
+        RoundNumber += 1
+        if(RoundNumber>1000) return
     }
 
-
 }
+
+FullMatch(3,playerNamesArr,playersPoints,-5,13)
